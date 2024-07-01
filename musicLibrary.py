9b0fc3108e86269e9d3fd204eceab6449fe396e8
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-07-01 15:52:43 +0530

    first commit

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "sage" : "https://youtu.be/_kUrW9SEaJc?si=tlu1nfdvwPc_jPS6",
+    "stealth" : "https://youtu.be/U47Tr9BB_wE?si=jUI6sQvDlJp8rDhV",
+    "frozen" : "https://youtu.be/ItUOIqeGzGc?si=0_LFF3u5ZcMWO3_X",
+    "march" : "https://youtu.be/EDgaPJYe4Tg?si=tvW_d0rpxZUnw908",
+    "skyfall" : "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D"
+}
